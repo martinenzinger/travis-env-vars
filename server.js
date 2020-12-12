470b30e04b831944a66269774ac58086af1eeb31
Martin Enzinger <martin.enzinger@gmail.com>
2020-12-12 03:41:24 +0100

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 
+const TEST_ENV_VAR = process.env.TEST_ENV_VAR;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log("This is an environment variable: ", process.env.TEST_ENV_VAR);
+    console.log("This is an environment variable: ", TEST_ENV_VAR);
     await sleep(5000);
   }
 }
